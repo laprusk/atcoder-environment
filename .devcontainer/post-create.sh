ce8828ffc3031e1c7c9a0d9bc6548cc2c9c5ddee
laprusk <laprusk72@gmail.com>
2024-01-08 20:14:18 +0900

    Add mounts for online-judge-tools and acc-setting in devcontainer.json

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mkdir $(acc config-dir)/cpp
-cp .devcontainer/template.json $(acc config-dir)/cpp/
+mkdir -p $(acc config-dir)/cpp
+cp -n .devcontainer/template.json $(acc config-dir)/cpp/
 acc config default-template cpp
